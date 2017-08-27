9ec3198af2ff600286d22bf15f086757e6c73508
ryonakao <rodriguez.nak@gmail.com>
2017-08-27 13:17:41 +0900

    fix function to create pages when crawl

--- crawler.go
+++ crawler.go
@@ -8,6 +8,10 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
+	"webCrawler/models"
+
+	"gopkg.in/mgo.v2/bson"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -43,8 +47,9 @@ func (c *crawler) execute() {
 		select {
 		case res := <-c.res:
 			if res.err == nil {
-				fmt.Printf("%s\n", res.url)
-				fmt.Printf("%s\n", res.html)
+				//fmt.Printf("%s\n", res.url)
+				//fmt.Println("htmlは")
+				//fmt.Printf("%s\n", res.html)
 			} else {
 				fmt.Fprintf(os.Stderr, "Error %s\n%v\n", res.url, res.err)
 			}
@@ -69,14 +74,14 @@ func (c *crawler) execute() {
 		}
 	}
 	log.Println("スクレイピング完了")
-	fmt.Println(urlMap)
 }
 
+// Crawl クロールする
 func Crawl(url string, depth int, c *crawler) {
 	defer func() { c.quit <- 0 }()
 
 	// WebページからURLを取得
-	urls, html, err := Fetch(url)
+	urls, html, err := Fetch(url, depth)
 
 	// 結果送信
 	c.res <- &respons{
@@ -96,30 +101,46 @@ func Crawl(url string, depth int, c *crawler) {
 	}
 }
 
-func Fetch(u string) (urls []string, html string, err error) {
+// Fetch フェッチする
+func Fetch(u string, depth int) (urls []string, html string, err error) {
 	baseUrl, err := url.Parse(u)
 	if err != nil {
 		return
 	}
-
+	// html取得------------------------------
 	resp, err := http.Get(baseUrl.String())
 	if err != nil {
 		return
 	}
+	if depth == 1 {
+		html, err = getStaticFile(*resp)
+	}
 	defer resp.Body.Close()
 
-	// 取得したhtmlを文字列で確認したい時はこれ
-	//body, err := ioutil.ReadAll(resp.Body)
-	//buf := bytes.NewBuffer(body)
-	//html := buf.String()
-	//fmt.Println(html)
-	// ---------------
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	// スクレイピング------------------------------------
+	resp2, err := http.Get(baseUrl.String())
 	if err != nil {
 		return
 	}
+	defer resp2.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(resp2.Body)
+	if err != nil {
+		return
+	}
+	title, err := getTitle(doc)
+	//rank := 1 // 仮
+	//fmt.Println("titleは", title)
+	page := &models.Pages{
+		ID:        bson.NewObjectId(),
+		Title:     title,
+		URL:       u,
+		HTML:      html,
+		Rank:      1,
+		TargetDay: time.Now(),
+	}
+	fmt.Println("構造体は", page)
 
+	// 1回目のみなので関数分ける-----------------------------------------------------------------
 	urls = make([]string, 0)
 	doc.Find(".r").Each(func(_ int, srg *goquery.Selection) {
 		srg.Find("a").Each(func(_ int, s *goquery.Selection) {
@@ -127,32 +148,30 @@ func Fetch(u string) (urls []string, html string, err error) {
 			if exists {
 				reqUrl, err := baseUrl.Parse(href)
 				if err == nil {
-					// html取得------------------------------------------
-					html, err = getStaticFile(reqUrl.String())
-					if err != nil {
-						log.Fatal("エラー", err)
-					}
-					// -------------------------------------------------
 					urls = append(urls, reqUrl.String())
 				}
 			}
 		})
 	})
+	// -------------------------------------------------------------------------
 
 	return
 }
 
-func getStaticFile(reqURL string) (file string, err error) {
-	res, err := http.Get(reqURL)
-	if err != nil {
-		return
-	}
-	defer res.Body.Close()
+func getStaticFile(res http.Response) (file string, err error) {
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		return
 	}
 	buf := bytes.NewBuffer(body)
 	file = buf.String()
+	return
+}
+
+func getTitle(doc *goquery.Document) (title string, err error) {
+	doc.Find("title").Each(func(_ int, srg *goquery.Selection) {
+		title = srg.Text()
+	})
+
 	return
 }
